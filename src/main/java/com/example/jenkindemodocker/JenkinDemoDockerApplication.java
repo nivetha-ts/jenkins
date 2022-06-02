a33d6b8f7e177d090edc9e8005264eb2b5f1a98a
nivetha-ts <tsnivethabtechit@gmail.com>
2022-06-02 13:22:22 +0530

    files added

---
+++ src/main/java/com/example/jenkindemodocker/JenkinDemoDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkindemodocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinDemoDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinDemoDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkindemodocker/JenkinDemoDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkindemodocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinDemoDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
